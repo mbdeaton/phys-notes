b01c8b678f816511edef29dfcdb2a80addb52b41
Brett Deaton <brett.deaton@gmail.com>
2026-05-19 18:27:21 -0700

    Convert eslint config to ts-only

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{ts,tsx}"],
+    ignores: ["**/*.{js,jsx}"],
     extends: [
-      js.configs.recommended,
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      "plugin:prettier/recommended",
     ],
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
     },
   },
-])
+]);
